a9a7b7eeca6f047fca3a17f0bd729cd01507cc43
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-09 19:46:41 -0400

    Add data type variables and sample data to types.js

--- dist/types.js
+++ dist/types.js
@@ -1,1 +1,8 @@
 console.log("Data Types in TypeScript");
+var age = 30;
+var userName = "John Doe";
+var isSenior = true;
+var person = { name: "Jane", age: 25 };
+var hobbies = ["Reading", "Traveling", "Swimming"];
+var scores = [95, 85, 76];
+var fruits = [{ name: "Apple" }, { name: "Banana" }];
