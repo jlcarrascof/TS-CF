a8416f15577279dbc0dc2ee3c27974df65ec3a60
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-12 22:48:28 -0400

    Refactor Users enum to assign explicit numeric values to each user type

--- basics/enums.ts
+++ basics/enums.ts
@@ -1,8 +1,8 @@
 enum Users {
-    NormalUser,
-    PayedUser,
-    AdminUser,
-    MegaUser,
+    NormalUser = 5,
+    PayedUser = 7,
+    AdminUser = 9,
+    MegaUser = 11,
 } 
 
 const myUser = Users.AdminUser;
