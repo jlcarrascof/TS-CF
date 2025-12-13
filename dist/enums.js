9dcf90f1879131c943187b93147c1490a208a7fc
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-12 23:02:20 -0400

    Add Users2 enum for string representation of user types

--- dist/enums.js
+++ dist/enums.js
@@ -5,5 +5,14 @@ var Users;
     Users[Users["AdminUser"] = 9] = "AdminUser";
     Users[Users["MegaUser"] = 11] = "MegaUser";
 })(Users || (Users = {}));
+var Users2;
+(function (Users2) {
+    Users2["NormalUser"] = "normal";
+    Users2["PayedUser"] = "payed";
+    Users2["AdminUser"] = "admin";
+    Users2["MegaUser"] = "mega";
+})(Users2 || (Users2 = {}));
 var myUser = Users.AdminUser;
 console.log(myUser);
+var myUser2 = Users2.AdminUser;
+console.log(myUser2);
