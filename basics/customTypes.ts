01ef518861bae2300fc682e36dc6e23e91212858
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-14 12:51:26 -0400

    Fix formatting and add missing newlines in customTypes.ts

--- basics/customTypes.ts
+++ basics/customTypes.ts
@@ -2,9 +2,16 @@ type Person = {
     name: string;
     code: string | number;
     description?: string;    
-}
+};
 
 const newPerson: Person = {
     code: "H",
     name: "Kevin",
-}
+};
+
+type ServiceResponse = string | null | number | undefined;
+
+type userCharges = "admin" | "normal" | "superUser";
+let response: ServiceResponse;
+
+const myUserType: userCharges = 'superUser';
