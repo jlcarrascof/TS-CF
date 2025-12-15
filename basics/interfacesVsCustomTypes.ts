5ec6547bcb5ea4810f4537fc9d77dd5c8590b46a
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-14 22:55:57 -0400

    Add User and Admin type definitions with ServiceResponseToken and UserTypes

--- basics/interfacesVsCustomTypes.ts
+++ basics/interfacesVsCustomTypes.ts
@@ -12,3 +12,17 @@ const newEmployee: Employee = {
     age: 40,
     charge: "",
 }
+
+// Union Types ....
+
+type User = {
+    id: string;
+}
+
+type Admin = User & Person;
+
+type ServiceResponseToken = string | null | undefined | number;
+
+type UserTypes = "admin" | "superUser" | 0;
+
+const myUser: UserTypes = 0;
