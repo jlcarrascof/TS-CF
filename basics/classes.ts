5e53698ff1875b42e6c5d29c57619d4729b0654e
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-15 22:03:14 -0400

    Enhance Animal class sayHi method to include age in output

--- basics/classes.ts
+++ basics/classes.ts
@@ -1,14 +1,13 @@
 (() => {
     class Animal {
         name: "Generic Animal";
-
+        private age = 10;
         sayHi() {
-            console.log("Grrr");
+            console.log("Grrr", this.age);
         }
     }
 
     const myAnimal: Animal = new Animal();
     myAnimal.sayHi();    
-
 
 })();
