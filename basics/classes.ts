a138c6ffe83b67d522c76493cd4435778597130a
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-16 11:37:55 -0400

    Remove commented-out code for Animal and Dog classes to improve readability

--- basics/classes.ts
+++ basics/classes.ts
@@ -1,24 +1,3 @@
-/* (() => {
-    class Animal {
-        name: "Generic Animal";
-        private age = 10;
-        sayHi() {
-            console.log("Grrr", this.age);
-        }
-    }
-
-    class Dog extends Animal {
-        type: "German Sheld";
-    }
-
-    const myAnimal: Animal = new Animal();
-    myAnimal.sayHi();    
-
-    const myDog: Dog = new Dog();
-    console.log(myDog.name);
-
-})(); */
-
 class Animal {
     name = "Generic Animal";
     private age = 10;
