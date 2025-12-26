7fdf3376199a3b7e7cfade9bff21ba7212cb71ce
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-25 23:11:03 -0400

    Refactor index.ts to demonstrate alternative import style for utilities

--- advanced/modules/index.ts
+++ advanced/modules/index.ts
@@ -1,7 +1,16 @@
+/* Traditional way
 import { generateId, PI } from "./utils.module";
 import { User as Person } from './utils.module';
 
 const myNumber = 10 * PI;
 
 const myUser: Person = { id: generateId.toString(), name: 'Kevin'};
+*/
+
+/* Another way */
+import * as Utils from './utils.module';
+
+const myNumber = 10 * Utils.PI;
+
+const myUser: Utils.User = { id: Utils.generateId.toString(), name: 'Kevin'};
 
