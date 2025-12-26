a3267ee6ac908b3e80f4357a693a045cc4fbe315
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-25 23:07:15 -0400

    Refactor import statements and update myUser type to use Person alias

--- advanced/modules/index.ts
+++ advanced/modules/index.ts
@@ -1,6 +1,7 @@
-import { generateId, PI, User } from "./utils.module";
+import { generateId, PI } from "./utils.module";
+import { User as Person } from './utils.module';
 
 const myNumber = 10 * PI;
 
-const myUser: User = { id: generateId.toString(), name: 'Kevin'};
+const myUser: Person = { id: generateId.toString(), name: 'Kevin'};
 
